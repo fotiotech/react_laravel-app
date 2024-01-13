e96845b4c789f67484fa4e66b7230597ddf21c27
dylan/ <113419820+fotiotech@users.noreply.github.com>
2024-01-13 17:33:40 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,24 +7,23 @@ import Product from "./pages/Product.jsx";
 import CheckOut from "./pages/CheckOut.jsx";
 
 const App = () => {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<Home />}>
-          <Route index element={<ErrorPage />} />
-        </Route>
-        {/* <Route path="/login" element={<LoginPage />} />
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="*" element={<ErrorPage />} />
+                {/* <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/account" element={<ProfilePage />} /> */}
-        <Route path="/detail/:productid" element={<DetailPage />} />
-        <Route path="/product" element={<Product />} />
-        <Route path="/search/:q" element={<Search />} />
-        <Route path="/check-out" element={<CheckOut />} />
-        {/* <Route path="/account/bookings" element={<BookingsPage />} />
+                <Route path="/detail/:productid" element={<DetailPage />} />
+                <Route path="/product" element={<Product />} />
+                <Route path="/search" element={<Search />} />
+                <Route path="/check-out" element={<CheckOut />} />
+                {/* <Route path="/account/bookings" element={<BookingsPage />} />
           <Route path="/account/bookings/:id" element={<BookingPage />} /> */}
-      </Routes>
-    </>
-  );
+            </Routes>
+        </>
+    );
 };
 
 export default App;
