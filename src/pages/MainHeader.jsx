3ed04269c4ac7f38d4e559f62e97ceb7e463342f
dylan/ <113419820+fotiotech@users.noreply.github.com>
2024-01-13 17:35:10 +0100

    Update MainHeader.jsx

--- src/pages/MainHeader.jsx
+++ src/pages/MainHeader.jsx
@@ -3,44 +3,47 @@ import { Link } from "react-router-dom";
 import FetchData from "./hooks/FetchData";
 
 const MainHeader = () => {
-  const [Product, setProduct] = useState(null);
+    const [Product, setProduct] = useState(null);
 
-  FetchData("HeaderProduct.json", setProduct);
+    FetchData("HeaderProduct.json", setProduct);
 
-  return (
-    <>
-      <div>
-        <div>
-          <header
-            className={`h-[700px] bg-[url('/boy-long_sleeve.jpg')] bg-cover bg-center w-full`}
-          >
-            <div className="bg-gradient-to-t from-[#ffffffff] to-[#ffffff00] h-[700px] relative  w-full">
-              <div className=" absolute flex flex-wrap  justify-between items-center top-72 h-[720px] mx-20 p-3 bg-none">
-                {Product ? (
-                  Product.map((product) => (
-                    <div
-                      key={product.id}
-                      className=" w-72 h-80 p-5 shadow bg-white rounded"
+
+    return (
+        <>
+            <div>
+                <div>
+                    <header
+                        className={`h-[700px] max-sm:h-[500px] bg-[url('/boy-long_sleeve.jpg')] bg-cover bg-center w-full`}
                     >
-                      <Link to={"/detail/" + product.id}>
-                        <img
-                          src={product.image}
-                          className=" w-64 h-64 m-0 m-auto"
-                        />
-                        <p className="">{product.name}</p>
-                      </Link>
-                    </div>
-                  ))
-                ) : (
-                  <p>data is Loading...</p>
-                )}
-              </div>
+                        <div className="bg-gradient-to-t from-[#ffffffff] to-[#ffffff00] h-[700px] max-sm:h-[500px]  relative  w-full">
+                            <div className=" absolute flex flex-wrap max-sm:flex-nowrap max-sm:w-full max-sm:whitespace-nowrap max-sm:overflow-hidden max-sm:h-72 justify-between items-center top-72 max-sm:top-44 h-[720px] max-sm:mx-0  mx-20 p-3 bg-none">
+                                {Product ? (
+                                    Product.map((product) => (
+                                        <div
+                                            key={product.id}
+                                            className=" w-72 max-sm:w-[48%] max-sm:m-2 max-sm:inline-block  h-80 max-sm:h-[250px] p-5 max-sm:p-2  shadow bg-white rounded"
+                                        >
+                                            <Link to={"/detail/" + product.id}>
+                                                <img
+                                                    src={product.image}
+                                                    className=" w-64 h-64 max-sm:h-36 m-0 m-auto"
+                                                />
+                                                <p className="">
+                                                    {product.name}
+                                                </p>
+                                            </Link>
+                                        </div>
+                                    ))
+                                ) : (
+                                    <p>data is Loading...</p>
+                                )}
+                            </div>
+                        </div>
+                    </header>
+                </div>
             </div>
-          </header>
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 };
 
 export default MainHeader;
