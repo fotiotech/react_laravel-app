de1e1fcdead7c081a02030226ae1f0b63a553d79
dylan/ <dilanefotio14@gmail.com>
2024-01-19 20:11:24 +0100

    updated

--- src/pages/admin/Admin.tsx
+++ src/pages/admin/Admin.tsx
@@ -1,22 +1,36 @@
-// import { Link } from "react-router-dom";
-import Header from "../Header";
 import Products from "./Products";
 import Dashbord from "./Dashbord";
 import Command from "./Command";
-import { useCallback, useState } from "react";
+import { useState } from "react";
 import React from "react";
+import { Close, Menu } from "@mui/icons-material";
 
 const Admin = () => {
   var [showComponent, setShowComponent] = useState(<Dashbord />);
-
+  const [meNu, setMeNu] = useState("max-sm:-left-20");
 
   return (
     <div className="">
-      <Header />
+      <div
+        onClick={() => setMeNu("max-sm:left-0")}
+        className="invisible max-sm:visible"
+      >
+        <Menu />
+      </div>
       <div className="flex px-20 w-full max-sm:w-full relative max-sm:px-1 h-full overflow-hidden max-sm:overflow-x-clip bg-slate-100">
-        <div className="w-[20%] py-3 border h-[620px] max-sm:absolute max-sm:-ml-12 overflow-auto scrollbar scrollbar-thin scrollbar-track-red-500  bg-white">
+        <div
+          className={`w-[20%] max-sm:w-80 py-3 border h-[620px] max-sm:absolute ${meNu}  overflow-auto scrollbar scrollbar-thin scrollbar-track-red-500  bg-white`}
+        >
+          <div
+            onClick={() => setMeNu("max-sm:-left-20")}
+            className=" invisible max-sm:visible ml-48"
+          >
+            <Close />
+          </div>
           <div>
-            <h2 className="p-3 font-semibold">Menu</h2>
+            <div>
+              <h2 className="p-3 font-semibold">Menu</h2>
+            </div>
             <h2
               onClick={() => setShowComponent(<Dashbord />)}
               className="text-center font-semibold border"
@@ -154,10 +168,16 @@ const Admin = () => {
             </h2>
           </div>
         </div>
-        <div className="w-[60%] h-full max-sm:w-full bg-white border">{showComponent}</div>
+        <div className="w-[60%] h-full max-sm:w-full bg-white border">
+          {showComponent}
+        </div>
         <div className="w-[20%] max-sm:absolute max-sm:ml-96">
           <form>
-            <input title="input" type="text" className="w-full h-20 rounded-3xl border" />
+            <input
+              title="input"
+              type="text"
+              className="w-full h-20 rounded-3xl border"
+            />
             <button title="button" />
           </form>
         </div>
